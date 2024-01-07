efabafee02aa0847afee759905e1811fd93cfdff
Alex Nielsen <alex@bigal.org>
2024-01-07 16:37:42 -0500

    started on expanding business model

--- backend/models/businessModel.js
+++ backend/models/businessModel.js
@@ -1,31 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const businessSchema = new Schema(
   {
-    title: {
-      type: String,
-      required: true,
-    },
-    shortDescription: {
-      type: String,
-      required: true,
-    },
-    owner: {
-      type: String,
-      required: true,
-    },
-    isPublished: {
-      type: Boolean,
-      required: true,
-    },
-    user_id: {
-      type: String,
-      required: true,
-    },
+    title: { type: String, required: true },
+    shortDescription: { type: String, required: true },
+    owner: { type: String, required: true },
+    isPublished: { type: Boolean, required: true },
+    user_id: { type: String, required: true },
+    campus: { type: String, required: true },
+    longDescription: { type: String, required: true },
   },
   { timestamps: true }
-)
+);
 
-module.exports = mongoose.model('Business', businessSchema)
+module.exports = mongoose.model('Business', businessSchema);
